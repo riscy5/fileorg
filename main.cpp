24a0b86d6b9e78b7b2e903e86fd51dc451430f12
unknown <gmmorgado2006@gmail.com>
2026-09-11 18:40:42 +0400

    Created file struct, string converter for path types, basic printing vector of file type

--- main.cpp
+++ main.cpp
@@ -1,1 +1,53 @@
+// Z:\fileorg_experimentation will be used for desktop directory experimentation
 
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <iostream>
+#include <vector>
+
+using namespace std::filesystem;
+using namespace std;
+
+// const so it doesn't modify
+// & call by reference to indicate file won't be modified due to const
+
+string extractExtension(const path& f) {
+    return f.extension().string();
+}
+
+string extractFilename(const path& f) {
+    return f.filename().string();
+}
+
+void directoryfilesToVector(vector<file> &v, const path &d) {
+    file input;
+    
+    for(path in : directory_iterator(d)) {
+        input.filename = extractFilename(in);
+        input.extension = extractExtension(in);
+        v.insert(v.end(), input);
+    }
+}
+
+struct file {
+    string filename;
+    string extension;
+};
+
+int main() {
+    path maindir = "Z:\fileorg_experimentation";
+
+    if(!exists(maindir)) {
+        cout << "Directory does not exist." << endl;
+        return 1;
+    }
+
+    vector<file> allFiles;
+
+    directoryfilesToVector(allFiles, maindir);
+
+    for(int i = 0; i < allFiles.size(); i++) {
+        cout << "File " << i + 1 << ": " << allFiles[i].filename << allFiles[i].extension;
+    }
+}

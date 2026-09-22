a6daeba5f6b18c3350f9e98b47231c40579718f3
Giovanni Morgado <gmmorgado2006@gmail.com>
2026-09-22 19:52:46 +0400

    Removed .ini from a categories map In any directory, moving .ini files is dangerous. Therefore, it has been removed to prevent accidental renaming.

--- categories.h
+++ categories.h
@@ -15,7 +15,7 @@ const map<string, vector<string>> categoryGroups = {
     {"Videos",    {".mp4", ".mov", ".avi", ".mkv", ".wmv", ".flv", ".webm", ".m4v", ".mpg", ".mpeg", ".3gp", ".ts", ".m2ts", ".vob", ".ogv"}},
     {"Audio",     {".mp3", ".wav", ".flac", ".aac", ".ogg", ".wma", ".m4a", ".aiff", ".mid", ".midi", ".opus", ".amr"}},
     {"Archives",  {".zip", ".rar", ".7z", ".tar", ".gz", ".bz2", ".xz", ".iso", ".cab", ".tgz", ".zst", ".lz", ".arj"}},
-    {"Code",      {".cpp", ".c", ".h", ".hpp", ".py", ".java", ".js", ".html", ".css", ".php", ".rb", ".go", ".rs", ".swift", ".sh", ".bat", ".json", ".xml", ".yaml", ".yml", ".sql", ".cs", ".kt", ".lua", ".pl", ".scss", ".sass", ".jsx", ".tsx", ".vue", ".ini", ".toml", ".r", ".m"}},
+    {"Code",      {".cpp", ".c", ".h", ".hpp", ".py", ".java", ".js", ".html", ".css", ".php", ".rb", ".go", ".rs", ".swift", ".sh", ".bat", ".json", ".xml", ".yaml", ".yml", ".sql", ".cs", ".kt", ".lua", ".pl", ".scss", ".sass", ".jsx", ".tsx", ".vue", ".toml", ".r", ".m"}},
     {"Executables", {".exe", ".msi", ".app", ".dmg", ".deb", ".rpm", ".apk", ".bin", ".com", ".jar", ".run", ".appimage", ".msix"}},
     {"Fonts",     {".ttf", ".otf", ".woff", ".woff2", ".pfb", ".pfm", ".eot"}},
     {"Shortcuts", {".lnk", ".url", ".pif"}},
